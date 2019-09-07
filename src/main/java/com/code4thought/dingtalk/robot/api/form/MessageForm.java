38ee3af913503ccb254867396c5848f85fb2beeb
kangliqi <kangliqi@ifreecomm.com>
2019-09-08 02:37:46 +0800

    添加消息翻译转发API

---
+++ src/main/java/com/code4thought/dingtalk/robot/api/form/MessageForm.java
@@ -0,0 +1,30 @@
+package com.code4thought.dingtalk.robot.api.form;
+
+import javax.validation.constraints.NotBlank;
+
+public class MessageForm {
+
+    private String title;
+
+    @NotBlank(message = "Text must not be null.")
+    private String text;
+
+    private Boolean translate;
+    private Boolean atAll;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Boolean getTranslate() {
+        return translate;
+    }
+
+    public Boolean getAtAll() {
+        return atAll;
+    }
+}
